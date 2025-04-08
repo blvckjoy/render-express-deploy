2b83166fbce17bf3d2edcd32907200cdcaee5a87
blvckjoy <sirjeggz@gmail.com>
2025-04-08 13:09:39 +0100

    Refactor userRouter

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const userRoutes = require("./routes/userRoutes");
 
 app.use(express.json());
 
-// Root route
-app.get("/", (req, res) => {
-   res.send("Users' API");
-});
-
-app.use("/users", userRoutes);
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
 
 app.listen(PORT, () => {
    console.log(`Listening on port ${PORT}`);
